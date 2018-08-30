1ab5174713a76a1653c29976c6995a33ecc815e5
Len <40214713+lenholacode@users.noreply.github.com>
2018-08-30 11:08:06 -0500

    Update GroceryItem.jsx

--- client/src/components/GroceryItem.jsx
+++ client/src/components/GroceryItem.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-
 const GroceryItem = (props) => (
-  <div>
-    <span>{props.item.description}</span>
-    <span> {props.item.quantity} </span>
-  </div>
+  <div></div>
 )
-
 export default GroceryItem;
