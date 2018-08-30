61232661f262a9ba209c9169a5a117174386d70b
Len <40214713+lenholacode@users.noreply.github.com>
2018-08-30 11:11:52 -0500

    Update AddGrocery.jsx

--- client/src/components/AddGrocery.jsx
+++ client/src/components/AddGrocery.jsx
@@ -3,44 +3,18 @@ import React from 'react';
 class AddGrocery extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { 
     this.state = {
-      description: '',
-      quantity: ''
     }
-    this.add = this.add.bind(this);
-    this.updateDescription = this.updateDescription.bind(this);
-    this.updateQuantity = this.updateQuantity.bind(this);
-
   }
-
-  updateDescription(e) {
-    this.setState({
-      description: e.target.value
-    })
-  }
-
-  updateQuantity(e) {
-    this.setState({
-      quantity: e.target.value
-    })
-  }
-
-  add() {
-    this.props.addItem(this.state.description, this.state.quantity);
-    this.setState({
-      description: '',
-      quantity: ''
-    })
-  }
-
   render () {
+    return null;
     return (<div>
-      Description: <input onChange={this.updateDescription} value={this.state.description}></input>
+      Description: <input ></input>
       <br />
-      Quantity: <input onChange={this.updateQuantity} value={this.state.quantity}></input>
-      <button onClick={this.add}>Add Grocery</button>
+      Quantity: <input ></input>
+      <button>Add Grocery</button>
     </div>);
   }
 }
-
 export default AddGrocery;
